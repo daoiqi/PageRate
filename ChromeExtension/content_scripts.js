3e814c071a0d757881be2f9bbb6f00a29f4d2365
daoiqi <daoiqi@gmaill.com>
2014-08-05 23:43:14 +0800

    提交css文件

--- ChromeExtension/content_scripts.js
+++ ChromeExtension/content_scripts.js
@@ -1,8 +1,36 @@
+loadCSS = function(href) {
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.href = href;
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    head.appendChild(link);
+ };
+
+/**
+ * function to load a given js file 
+ */ 
+loadJS = function(src) {
+    var jsLink = $("<script type='text/javascript' src='"+src+"'>");
+    $("head").append(jsLink); 
+}; 
+
+
+
+
 
 
 $(function($,win) {
+	var css = chrome.extension.getURL("css/style.css");
+	console.log(css);
+	loadCSS(css);
+
+
 	function insertRate(argument) {
-		$div = $("<div class='a'>123123</div>");
+		$div = $("<div class='daoiqi_pagerate'><div class='daoiqi_pagerate_rankButton'>评分</div></div>");
 		$div.appendTo("body");
+		console.log(123);
+		console.log($div);
 	} insertRate();
 }($,window));
+
